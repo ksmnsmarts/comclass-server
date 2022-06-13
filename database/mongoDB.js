5ece73386f384cadfc1af8ffa0657aebb8bc9660
ksmnsmarts <ksm@nsmarts.co.kr>
2022-06-13 18:19:54 +0900

    first

---
+++ database/mongoDB.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const mongApp = {}; 
+mongApp.appSetObjectId = function (app) {
+	app.set('ObjectId', mongoose.Types.ObjectId);
+	console.log('complete to set mongoose ObjectId');
+}
+
+main().catch(err => console.log(err));
+
+async function main() {
+
+	// test-potatocs , potatocs 
+	await mongoose.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}).then( ()=> {
+		createSchema();
+		console.log('Database Connected');
+	});
+}
+
+function createSchema() {
+	const dbModels = {};
+
+	dbModels.Admin = require('../models/admin_schema');
+	
+
+	global.DB_MODELS = dbModels;
+}
+
+module.exports = mongApp;
---
+++ routes/api/v1/index.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const { isAuthenticated } = require('../../../middlewares/auth');
+
+// AUTH
+const auth = require('./auth/auth_index');
+const adAuth = require('./admin/adAuth/adAuth_index');
+
+// AUTH
+const user = require('./user/user_index');
+
+const admin = require('./admin/admin_index');
+
+
+
+
+/*-----------------------------------
+	not needed to verify
+-----------------------------------*/
+router.use('/auth', auth);
+router.use('/adAuth', adAuth);
+
+/*-----------------------------------
+	Token verify
+-----------------------------------*/
+router.use(isAuthenticated);
+
+/*-----------------------------------
+	API
+-----------------------------------*/
+router.use('/admin', admin);
+router.use('/user', user);
+
+
+/*-----------------------------------
+	Nsmarts
+-----------------------------------*/
+
+
+module.exports = router;
